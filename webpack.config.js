dc3c5696df4aaab44b08fb13bd4216771bfb239f
Aleksandr Gordeev <alexander.gordeev@vk.team>
2023-07-09 17:04:54 +0300

    Добавил линтеры и почистил код

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,52 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const EslintWebpackPlugin = require('eslint-webpack-plugin');
+const StylelintWebpackPlugin = require('stylelint-webpack-plugin');
 
 module.exports = {
-  mode: process.env.NODE_ENV || 'production',
-  entry: './src/index.tsx',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.[contenthash].js',
-    publicPath: '/',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        use: 'babel-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(ts|tsx)$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.js', '.ts', '.tsx'],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
-  ],
-  devServer: {
-    client: {
-      overlay: false,
+    mode: process.env.NODE_ENV || 'production',
+    entry: './src/index.tsx',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.[contenthash].js',
+        publicPath: '/',
     },
-    open: true,
-  },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(ts|tsx)$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.js', '.ts', '.tsx'],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
+        new EslintWebpackPlugin({
+            files: '{**/*,*}.{tsx,ts,js}',
+        }),
+        new StylelintWebpackPlugin({
+            files: '{**/*,*}.css',
+        }),
+    ],
+    devServer: {
+        client: {
+            overlay: false,
+        },
+        open: true,
+    },
 };
