e01e54041a7b479204656d43859cfee6ff2d3b94
Aleksandr Gordeev <alexander.gordeev@vk.team>
2023-07-10 19:07:25 +0300

    Обновляем заголовок в поиске при новом запросе

--- src/components/UsersSearchPage/UsersSearchPage.tsx
+++ src/components/UsersSearchPage/UsersSearchPage.tsx
@@ -1,16 +1,26 @@
 import React, { FC } from 'react';
-import { Await, useLoaderData, useSearchParams } from 'react-router-dom';
+import { Await, useLoaderData, useNavigation, useSearchParams } from 'react-router-dom';
 
 import { UsersList } from '../UsersList/UsersList';
 import { IUserCard } from '../../types';
 
 export const UsersSearchPage: FC = () => {
     const [searchParams] = useSearchParams();
+    const { state } = useNavigation();
     const data = useLoaderData() as { users: IUserCard[] };
 
+    const isLoading = state === 'loading';
+
+    const renderLoader = () => <h1 className="title">Ищем...</h1>;
+
     const renderComponent = (users: IUserCard[]) => {
+        if (isLoading) {
+            return renderLoader();
+        }
+
         const renderHeading = () => {
             const username = searchParams.get('query');
+
             return (
                 <h1 className="title">
                     {users.length ? `Пользователи по запросу ${username}` : `Ничего не найдено по запросу ${username}`}
@@ -25,10 +35,11 @@ export const UsersSearchPage: FC = () => {
             </>
         );
     };
+
     return (
         <main>
             <div className="container">
-                <React.Suspense fallback={<h1 className="title">Ищем...</h1>}>
+                <React.Suspense fallback={renderLoader()}>
                     <Await resolve={data.users} errorElement={<>Error fetch data</>}>
                         {renderComponent}
                     </Await>
